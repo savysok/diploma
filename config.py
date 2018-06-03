5ec604209357eefbadcd82d58103da5769ebf186
savysok <info@savysok.gr>
2018-06-03 13:01:51 +0300

    moved the path variables to seperate file

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+# PATHS
+# Set the absolute directories of the folders as variables
+data_dir = os.path.abspath(__file__ + '/../data') + '/'
+info_dir = os.path.abspath(__file__ + '/../info') + '/'
+models_dir = os.path.abspath(__file__ + '/../models') + '/'
+custom_dir = os.path.abspath(__file__ + '/../custom') + '/'
+#textures_dir = os.path.abspath(__file__+'/../textures') + '/' ### Not needed for now
+screenshot_dir = os.path.abspath(__file__+'/../screenshots') + '/'
