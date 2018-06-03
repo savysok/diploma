dfa7c32a51727e0cc86ec8a5f2057bca1e6291f6
savysok <info@savysok.gr>
2018-06-03 13:14:41 +0300

    moved the screenshot function to a new file

--- scripts/main.py
+++ scripts/main.py
@@ -817,45 +817,7 @@ def set_camera_position():
         ]
 
 
-# SCREENSHOT
-def take_screenshot():
-    """Function to take a screenshot.
-    Requires Imagemagick in Linux.
-    Windows support is in the TODO list.
-    """
-    controller = bge.logic.getCurrentController()
-    debug_print("The controller is:", controller)
-    own = controller.owner
-    debug_print("The owner is:", own)
-
-    scene = bge.logic.getCurrentScene()
-    debug_print("Current scene is:", scene)
-
-    mouse_over_button = own.sensors["mouse_over_button"]
-    left_click_button = own.sensors["left_click_button"]
-
-    date = time.strftime("%Y%m%d_%H%M%S_")
-    screenshot = screenshot_dir+date+'scrnsht.jpg'
-
-    screenshot_args = [
-        'import',
-        '-window',
-        'root',
-        '-resize',
-        '1920x1280',
-        '-delay',
-        '200',
-        screenshot
-        ]
-
-    if mouse_over_button.positive and left_click_button.positive:
-        debug_print("Taking screenshot...")
-        subprocess.run(screenshot_args)
-
-
 # PHASE 2: MAIN
-
-
 def main():
     """The main function (MAIN scene) that controls everything."""
     controller = bge.logic.getCurrentController()
---
+++ scripts/screenshot.py
@@ -0,0 +1,36 @@
+import bge
+import subprocess
+import time
+from config import screenshot_dir
+
+
+def take_screenshot():
+    """Function to take a screenshot.
+    Requires Imagemagick in Linux.
+    Windows support is in the TODO list.
+    """
+    controller = bge.logic.getCurrentController()
+    own = controller.owner
+
+    scene = bge.logic.getCurrentScene()
+
+    mouse_over_button = own.sensors["mouse_over_button"]
+    left_click_button = own.sensors["left_click_button"]
+
+    date = time.strftime("%Y%m%d_%H%M%S_")
+    screenshot = screenshot_dir+date+'scrnsht.jpg'
+
+    screenshot_args = [
+        'import',
+        '-window',
+        'root',
+        '-resize',
+        '1920x1280',
+        '-delay',
+        '200',
+        screenshot
+        ]
+
+    if mouse_over_button.positive and left_click_button.positive:
+        print("Taking screenshot...")
+        subprocess.run(screenshot_args)
