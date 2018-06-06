78ff0c6db82c8c35b67f999d1f2a3961688cf607
savysok <info@savysok.gr>
2018-06-06 12:11:16 +0300

    moved the variables outside the function

--- scripts/cube_rotation.py
+++ scripts/cube_rotation.py
@@ -1,13 +1,15 @@
 import bge
 from random import randint
 
+x = randint(1,5)/1000
+y = randint(1,5)/1000
+z = randint(1,5)/1000
+
 def rotate_cube():
     
     controller = bge.logic.getCurrentController()
     own = controller.owner
 
-    x = randint(1,5)/1000
-    y = randint(1,5)/1000
-    z = randint(1,5)/1000
+
     
     own.applyRotation([x,y,z])
